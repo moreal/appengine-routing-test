972086a30103b968afc0011bdba48c407731f0ce
moreal <dev.moreal@gmail.com>
2019-04-08 19:14:53 +0900

    Update script/test.py to check status_codes value

--- script/test.py
+++ script/test.py
@@ -6,6 +6,8 @@ status_codes = 0
 status_codes += requests.get(URL + 'hello').status_code
 status_codes += requests.get(URL + 'world').status_code
 
+print "status_codes value is ", status_codes
+
 if status_codes != 400:
     exit(-1)
 else:
